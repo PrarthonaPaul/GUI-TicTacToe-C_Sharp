8768798817e6edcf0f16a10fae037121a6a1ce57
Prarthona Paul <83880425+PrarthonaPaul@users.noreply.github.com>
2021-05-11 17:56:05 -0400

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace TicTacToe
+{
+    partial class frmTicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTicTacToe));
+            this.lblTopLeft = new System.Windows.Forms.Label();
+            this.lblTopMid = new System.Windows.Forms.Label();
+            this.lblTopRight = new System.Windows.Forms.Label();
+            this.lblCentreRight = new System.Windows.Forms.Label();
+            this.lblCentre = new System.Windows.Forms.Label();
+            this.lblCentreLeft = new System.Windows.Forms.Label();
+            this.lblBottomRight = new System.Windows.Forms.Label();
+            this.lblBotMid = new System.Windows.Forms.Label();
+            this.lblBotLeft = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRound = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTopLeft
+            // 
+            this.lblTopLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopLeft.Location = new System.Drawing.Point(28, 53);
+            this.lblTopLeft.Name = "lblTopLeft";
+            this.lblTopLeft.Size = new System.Drawing.Size(110, 100);
+            this.lblTopLeft.TabIndex = 0;
+            this.lblTopLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTopLeft.Click += new System.EventHandler(this.lblTopLeft_Click);
+            // 
+            // lblTopMid
+            // 
+            this.lblTopMid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopMid.Location = new System.Drawing.Point(144, 53);
+            this.lblTopMid.Name = "lblTopMid";
+            this.lblTopMid.Size = new System.Drawing.Size(110, 100);
+            this.lblTopMid.TabIndex = 1;
+            this.lblTopMid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTopMid.Click += new System.EventHandler(this.lblTopMid_Click);
+            // 
+            // lblTopRight
+            // 
+            this.lblTopRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTopRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopRight.Location = new System.Drawing.Point(260, 53);
+            this.lblTopRight.Name = "lblTopRight";
+            this.lblTopRight.Size = new System.Drawing.Size(110, 100);
+            this.lblTopRight.TabIndex = 2;
+            this.lblTopRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTopRight.Click += new System.EventHandler(this.lblTopRight_Click);
+            // 
+            // lblCentreRight
+            // 
+            this.lblCentreRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCentreRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCentreRight.Location = new System.Drawing.Point(260, 153);
+            this.lblCentreRight.Name = "lblCentreRight";
+            this.lblCentreRight.Size = new System.Drawing.Size(110, 100);
+            this.lblCentreRight.TabIndex = 5;
+            this.lblCentreRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCentreRight.Click += new System.EventHandler(this.lblCentreRight_Click);
+            // 
+            // lblCentre
+            // 
+            this.lblCentre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCentre.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCentre.Location = new System.Drawing.Point(144, 153);
+            this.lblCentre.Name = "lblCentre";
+            this.lblCentre.Size = new System.Drawing.Size(110, 100);
+            this.lblCentre.TabIndex = 4;
+            this.lblCentre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCentre.Click += new System.EventHandler(this.lblCentre_Click);
+            // 
+            // lblCentreLeft
+            // 
+            this.lblCentreLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCentreLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCentreLeft.Location = new System.Drawing.Point(28, 153);
+            this.lblCentreLeft.Name = "lblCentreLeft";
+            this.lblCentreLeft.Size = new System.Drawing.Size(110, 100);
+            this.lblCentreLeft.TabIndex = 3;
+            this.lblCentreLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCentreLeft.Click += new System.EventHandler(this.lblCentreLeft_Click);
+            // 
+            // lblBottomRight
+            // 
+            this.lblBottomRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBottomRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBottomRight.Location = new System.Drawing.Point(260, 253);
+            this.lblBottomRight.Name = "lblBottomRight";
+            this.lblBottomRight.Size = new System.Drawing.Size(110, 100);
+            this.lblBottomRight.TabIndex = 8;
+            this.lblBottomRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBottomRight.Click += new System.EventHandler(this.lblBottomRight_Click);
+            // 
+            // lblBotMid
+            // 
+            this.lblBotMid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBotMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBotMid.Location = new System.Drawing.Point(144, 253);
+            this.lblBotMid.Name = "lblBotMid";
+            this.lblBotMid.Size = new System.Drawing.Size(110, 100);
+            this.lblBotMid.TabIndex = 7;
+            this.lblBotMid.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBotMid.Click += new System.EventHandler(this.lblBotMid_Click);
+            // 
+            // lblBotLeft
+            // 
+            this.lblBotLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBotLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBotLeft.Location = new System.Drawing.Point(28, 253);
+            this.lblBotLeft.Name = "lblBotLeft";
+            this.lblBotLeft.Size = new System.Drawing.Size(110, 100);
+            this.lblBotLeft.TabIndex = 6;
+            this.lblBotLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBotLeft.Click += new System.EventHandler(this.lblBotLeft_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(394, 98);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(149, 39);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Clear Board";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(394, 143);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(149, 39);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "End Game";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnResults
+            // 
+            this.btnResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResults.Location = new System.Drawing.Point(394, 53);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(157, 39);
+            this.btnResults.TabIndex = 11;
+            this.btnResults.Text = "Check Results";
+            this.btnResults.UseVisualStyleBackColor = true;
+            this.btnResults.Click += new System.EventHandler(this.btnResults_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(394, 239);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(149, 38);
+            this.txt1.TabIndex = 12;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(394, 315);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(149, 38);
+            this.txt2.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(394, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Player1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(394, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Player 2:";
+            // 
+            // lblRound
+            // 
+            this.lblRound.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRound.Location = new System.Drawing.Point(141, 12);
+            this.lblRound.Name = "lblRound";
+            this.lblRound.Size = new System.Drawing.Size(253, 41);
+            this.lblRound.TabIndex = 16;
+            this.lblRound.Text = "Round 1";
+            this.lblRound.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmTicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(565, 365);
+            this.Controls.Add(this.lblRound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnResults);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblBottomRight);
+            this.Controls.Add(this.lblBotMid);
+            this.Controls.Add(this.lblBotLeft);
+            this.Controls.Add(this.lblCentreRight);
+            this.Controls.Add(this.lblCentre);
+            this.Controls.Add(this.lblCentreLeft);
+            this.Controls.Add(this.lblTopRight);
+            this.Controls.Add(this.lblTopMid);
+            this.Controls.Add(this.lblTopLeft);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmTicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTopLeft;
+        private System.Windows.Forms.Label lblTopMid;
+        private System.Windows.Forms.Label lblTopRight;
+        private System.Windows.Forms.Label lblCentreRight;
+        private System.Windows.Forms.Label lblCentre;
+        private System.Windows.Forms.Label lblCentreLeft;
+        private System.Windows.Forms.Label lblBottomRight;
+        private System.Windows.Forms.Label lblBotMid;
+        private System.Windows.Forms.Label lblBotLeft;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRound;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class frmTicTacToe : Form
+    {
+        bool player = true;     //sets the turn as a true or false, if true, it is player 1, if false, player 2
+        int num1 = 0, num2 = 0, round = 1;
+
+        public frmTicTacToe()
+        {
+            InitializeComponent();
+        }
+        
+        private void lblTopLeft_Click(object sender, EventArgs e)
+        {
+            placeMark(lblTopLeft);
+        }
+
+        private void lblTopMid_Click(object sender, EventArgs e)
+        {
+            placeMark(lblTopMid);
+        }
+
+        private void lblTopRight_Click(object sender, EventArgs e)
+        {
+            placeMark(lblTopRight);
+        }
+
+        private void lblCentreLeft_Click(object sender, EventArgs e)
+        {
+            placeMark(lblCentreLeft);
+        }
+
+        private void lblCentre_Click(object sender, EventArgs e)
+        {
+            placeMark(lblCentre);
+        }
+
+        private void lblCentreRight_Click(object sender, EventArgs e)
+        {
+            placeMark(lblCentreRight);
+        }
+
+        private void lblBotLeft_Click(object sender, EventArgs e)
+        {
+            placeMark(lblBotLeft);
+        }
+
+        private void lblBottomRight_Click(object sender, EventArgs e)
+        {
+            placeMark(lblBottomRight);
+        }
+
+        private void lblBotMid_Click(object sender, EventArgs e)
+        {
+            placeMark(lblBotMid);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            clearBoard(); 
+        }
+
+        private void btnResults_Click(object sender, EventArgs e)
+        {
+            if (lblTopLeft.Text == "O" && lblCentre.Text == "O" && lblBottomRight.Text == "O")
+                player1Wins(lblTopLeft, lblCentre, lblBottomRight);
+
+            else if (lblTopLeft.Text == "X" && lblCentre.Text == "X" && lblBottomRight.Text == "X")
+                player2Wins(lblTopLeft, lblCentre, lblBottomRight);
+
+            else if (lblTopRight.Text == "O" && lblCentre.Text == "O" && lblBotLeft.Text == "O")
+                player1Wins(lblTopRight, lblCentre, lblBotLeft);
+
+            else if (lblTopRight.Text == "X" && lblCentre.Text == "X" && lblBotLeft.Text == "X")
+                player2Wins(lblTopRight, lblCentre, lblBotLeft);
+
+            else if (lblTopRight.Text == "O" && lblCentreRight.Text == "O" && lblBottomRight.Text == "O")
+                player1Wins(lblTopRight, lblCentreRight, lblBottomRight);
+
+            else if (lblTopRight.Text == "X" && lblCentreRight.Text == "X" && lblBottomRight.Text == "X")
+                player2Wins(lblTopRight, lblCentreRight, lblBottomRight);
+
+            else if (lblTopLeft.Text == "O" && lblCentreLeft.Text == "O" && lblBotLeft.Text == "O")
+                player1Wins(lblTopLeft, lblCentreLeft, lblBotLeft);
+
+            else if (lblTopLeft.Text == "X" && lblCentreLeft.Text == "X" && lblBotLeft.Text == "X")
+                player2Wins(lblTopLeft, lblCentreLeft, lblBotLeft);
+
+            else if (lblTopLeft.Text == "O" && lblTopRight.Text == "O" && lblTopMid.Text == "O")
+                player1Wins(lblTopLeft, lblTopMid, lblTopRight);
+
+            else if (lblTopLeft.Text == "X" && lblTopMid.Text == "X" && lblTopRight.Text == "X")
+                player2Wins(lblTopLeft, lblTopMid, lblTopRight);
+
+            else if (lblCentreLeft.Text == "O" && lblCentre.Text == "O" && lblCentreRight.Text == "O")
+                player1Wins(lblCentreLeft, lblCentre, lblCentreRight);
+
+            else if (lblCentreLeft.Text == "X" && lblCentre.Text == "X" && lblCentreRight.Text == "X")
+                player2Wins(lblCentreLeft, lblCentre, lblCentreRight);
+
+            else if (lblBotLeft.Text == "O" && lblBotMid.Text == "O" && lblBottomRight.Text == "O")
+                player1Wins(lblBottomRight, lblBotMid, lblBotLeft);
+
+            else if (lblBotLeft.Text == "X" && lblBotMid.Text == "X" && lblBottomRight.Text == "X")
+                player2Wins(lblBotLeft, lblBotMid, lblBottomRight);
+
+            else if (lblTopMid.Text == "O" && lblCentre.Text == "O" && lblBotMid.Text == "O")
+                player1Wins(lblTopMid, lblCentre, lblBotMid);
+
+            else if (lblTopMid.Text == "X" && lblCentre.Text == "X" && lblBotMid.Text == "X")
+                player2Wins(lblTopMid, lblCentre, lblBotMid);
+            else
+                MessageBox.Show("NO WINNERS!", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clearBoard();
+            round++;
+            lblRound.Text = "Round " + round.ToString(); 
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            txt1.Text = null;
+            txt2.Text = null;
+            if(num1 > num2)
+                MessageBox.Show("Player 1 wins the game!", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else if(num2 > num1)
+                MessageBox.Show("Player 2 wins the game!", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("The game ends in a tie.", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            num1 = 0;
+            num2 = 0;
+            round = 1;
+            lblRound.Text = "Round " + round.ToString();
+        }
+
+        //This Function received the label that the user created and based on the player variable, places an O or and X ok the label
+        private void placeMark(Label currentPosition)
+        {
+            if (player)
+            {
+                currentPosition.Text = "O";  //Player 1 plays with O
+                currentPosition.Enabled = false; //disables the label so that it can not be clicked till a new game starts
+            }
+            else if (player == false)
+            {
+                currentPosition.Text = "X";  //Player 2 plays with X
+                currentPosition.Enabled = false;
+            }
+            player = !player; //Switches to the other player's turn
+        }
+        private void player1Wins(Label lbl1, Label lbl2, Label lbl3)
+        {
+            lbl1.ForeColor = Color.Red; //Changes text color to red if player 1 wins
+            lbl1.Enabled = true;
+            lbl2.ForeColor = Color.Red;
+            lbl2.Enabled = true;
+            lbl3.ForeColor = Color.Red;
+            lbl3.Enabled = true;
+            MessageBox.Show("Player 1 wins this Round!", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            num1++;
+            txt1.Text = num1.ToString();
+        }
+        private void player2Wins(Label lbl1, Label lbl2, Label lbl3)
+        {
+            lbl1.ForeColor = Color.BlueViolet; //Changes text color to blueviolet if player 2 wins
+            lbl1.Enabled = true;
+            lbl2.ForeColor = Color.BlueViolet;
+            lbl2.Enabled = true;
+            lbl3.ForeColor = Color.BlueViolet;
+            lbl3.Enabled = true;
+            MessageBox.Show("Player 2 wins this round!", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            num2++;
+            txt2.Text = num2.ToString();
+        }
+
+        private void clearBoard()
+        {
+            Label[] positionsOnBoard = { lblTopRight, lblTopLeft, lblTopMid, lblCentre, lblCentreLeft, lblCentreRight, lblBotLeft, lblBotMid, lblBottomRight };
+
+            for (int i = 0; i < positionsOnBoard.Length; i++)
+            {
+                positionsOnBoard[i].Enabled = true;
+                positionsOnBoard[i].Text = null;
+                player = true;
+            }
+        }
+    }
+}
